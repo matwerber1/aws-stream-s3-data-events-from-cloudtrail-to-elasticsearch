d057f062325a2dce57551fc8749578176438d5b7
Mat Werber <werberm@amazon.com>
2019-12-19 19:57:22 -0800

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-BUCKET=<EXISTING_BUCKET_FOR_DEPLOYMENT_ARTIFACTS>
+# EDIT THESE VALUES
+BUCKET=<EXISTING_BUCKET_FOR_DEPLOYMENT_ARTIFACTS>   # this is any bucket where the SAM CLI can deploy your CF artifacts
+ES_DOMAIN_NAME=<DOMAIN_NAME_OF_YOUR_ES_CLUSTER>     # this is the "domain name" you see in the elasticsearch console
+ES_ENDPOINT=<HTTPS_ENDPOINT_OF_YOUR_ES_CLUSTER>
+
+# DO NOT EDIT BELOW (unless you want to)
 STACK_NAME=s3-events-to-es
 
 # Install Lambda function dependencies
 sam build
+
+if [ $? -ne 0 ]; then
+    echo "SAM build failed"
+fi
 
 sam package \
     --s3-bucket $BUCKET \
     --template-file .aws-sam/build/template.yaml \
     --output-template-file packaged.yaml
 
+if [ $? -ne 0 ]; then
+    echo "SAM package failed"
+fi
+
 sam deploy \
     --template-file packaged.yaml \
     --stack-name $STACK_NAME \
-    --capabilities CAPABILITY_IAM
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides EsDomainName=$ES_DOMAIN_NAME EsEndpoint=$ES_ENDPOINT
   
+if [ $? -ne 0 ]; then
+    echo "SAM deployment failed"
+fi
---
+++ lambda/publish-to-elasticsearch/app.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import boto3
+import json
+import gzip
+import os
+from base64 import b64decode
+import requests
+from requests_aws4auth import AWS4Auth
+
+region = os.environ.get('ES_REGION')
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+host = os.environ.get('ES_ENDPOINT')
+index = os.environ.get('ES_INDEX')
+type = os.environ.get('ES_DOC_TYPE')
+url = host + '/' + index + '/' + type + '/'
+
+headers = {"Content-Type": "application/json"}
+
+def decompress(data) -> bytes:
+    return gzip.decompress(data)
+
+def decode_record(data: dict) -> dict:
+    x = decompress(b64decode(data['data']))
+    return json.loads(x.decode('utf8'))
+
+def decode_event(event: dict) -> dict:
+    return decode_record(event['awslogs'])
+
+def handler(event, ctx) -> None:
+    event = decode_event(event)
+    print(json.dumps(event))
+
+    logEvents = event['logEvents']
+    print("Log events:\n{}".format(logEvents))
+
+    count = 0
+    print('Processing records...')
+    for record in logEvents:
+        id = record['id']
+        print('Sending record {} to ElasticSearch...'.format(id))
+        document = json.loads(record['message'])
+        r = requests.put(url + id, auth=awsauth, json=document, headers=headers)
+        print('ES response:\n{}'.format(r))
+        count += 1
+    print('{} records posted to ElasticSearch.'.format(count))
+
+# Based on: 
+# https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-aws-integrations.html#es-aws-integrations-dynamodb-es
