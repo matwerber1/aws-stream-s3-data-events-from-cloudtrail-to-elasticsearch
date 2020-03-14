6a49afdbb4207a566e9bbc4d3607dd5e6201e9df
Adrian Drummond <adrian.drummond@gmail.com>
2020-03-13 23:27:40 -0700

    adding ElasticCloud option

--- README.md
+++ README.md
@@ -8,7 +8,15 @@ Provide an example of streaming S3 object-level logs from CloudTrail to Elastics
 2. Subscribe Lambda to CloudWatch log group. 
 3. Lambda receive(s) event(s), makes an API put request to Elasticsearch
 
+
+## Architecture
+
+![MacDown logo](diagram.png)
+
+
 ## Pre-requisites
+
+*Amazon Elasticsearch Service*
 
 1. You need an existing Amazon Elasticsearch cluster running in an existing VPC. 
 This should work with a non-Amazon Elasticsearch cluster too, but you may need
@@ -17,6 +25,13 @@ to tweak the CloudFormation template and/or Lambda function a bit.
 2. Install the AWS CLI
 
 3. Install the AWS SAM CLI
+
+*Elastic Cloud*
+
+1. You need an existing elasticloud account and running cluster. 
+
+2. Retrive the credentials and config data to populate the environment variables. 
+
 
 ## Cost
 
@@ -27,23 +42,28 @@ signficant amount of log data, which has both cost and performance consideration
 You may want to initially adjust the demo to only apply to certain lower-volume
 buckets and/or object filters. 
 
-## Deployment
+## Deployment 
 
 1. Git clone this repository
 
-    `git clone https://github.com/matwerber1/aws-stream-s3-data-events-from-cloudtrail-to-elasticsearch.git`
+    `git clone https://github.com/drumadrian/amazon-s3-access-logs-to-elasticsearch.git`
 
-2. Open `deploy.sh` and edit the values below: 
-
-    ```sh
-    # deploy.sh
-    BUCKET=<EXISTING_BUCKET_FOR_DEPLOYMENT_ARTIFACTS>
-    ES_DOMAIN_NAME=<DOMAIN_NAME_OF_YOUR_ES_CLUSTER>
-    ES_ENDPOINT=<HTTPS_ENDPOINT_OF_YOUR_ES_CLUSTER>
-    ```
-
-3. Run the deployment script: 
+2. Change directory to the SAM folder:	*amazon-s3-access-logs-to-elasticsearch/sam-app*
+	
+	and provide config data when prompted by SAM: 
 
     ```sh
-    ./deploy.sh
+	git clone https://github.com/drumadrian/amazon-s3-access-logs-to-elasticsearch.git    
+	cd amazon-s3-access-logs-to-elasticsearch/sam-app
+	sam validate
+	sam build
+	sam deploy --guided
     ```
+
+3. Put data into your Production bucket and view in Kibana: 
+
+	Import the provided dashboard (TBD): 
+	
+    ```sh 
+	kibana.json
+	```
---
+++ logmydata/logmydata.py
@@ -0,0 +1,163 @@
+from __future__ import print_function
+import json
+import urllib.parse
+import boto3
+from elasticsearch import Elasticsearch
+import requests
+from datetime import datetime
+from s3logparse import s3logparse
+import os
+from tempfile import NamedTemporaryFile
+import traceback
+from aws_xray_sdk import core
+core.patch_all()
+
+import gzip
+from base64 import b64decode
+from requests_aws4auth import AWS4Auth
+
+
+
+# Notes:
+# https://docs.aws.amazon.com/code-samples/latest/catalog/python-s3-get_object.py.html
+# https://forums.aws.amazon.com/thread.jspa?threadID=221549
+# https://stackoverflow.com/questions/32000934/python-print-a-variables-name-and-value
+# https://pypi.org/project/s3-log-parse/
+# https://www.geeksforgeeks.org/python-dictionary/
+# https://stackoverflow.com/questions/44381249/treat-a-string-as-a-file-in-python
+# https://github.com/elastic/elasticsearch-py
+# https://docs.aws.amazon.com/lambda/latest/dg/running-lambda-code.html
+
+
+print('Loading function')
+
+
+# Initialize boto3 client at global scope for connection reuse
+client = boto3.client('ssm')
+s3 = boto3.client('s3')
+
+
+
+
+
+
+
+def decompress(data) -> bytes:
+    return gzip.decompress(data)
+
+def decode_record(data: dict) -> dict:
+    x = decompress(b64decode(data['data']))
+    return json.loads(x.decode('utf8'))
+
+def decode_event(event: dict) -> dict:
+    return decode_record(event['awslogs'])
+
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    cloud_id_var = os.getenv('cloud_id')
+    http_auth_username = os.getenv('http_auth_username')
+    http_auth_password = os.getenv('http_auth_password')
+    index_name = os.getenv('index_name')
+    parent_stack_name = os.getenv('parent_stack_name')
+
+
+
+    try:
+        # Get all parameters for this app is not set using environment variables
+
+        param_details = client.get_parameter(Name='/' + parent_stack_name + '/cloud_id',WithDecryption=True)
+        if 'Parameter' in param_details and len(param_details.get('Parameter')) > 0 and cloud_id_var is "-":
+            for param in param_details.get('Parameter'):
+                cloud_id_var = param.get('Value')
+
+        param_details = client.get_parameter(Name='/' + parent_stack_name + '/http_auth_username',WithDecryption=True)
+        if 'Parameter' in param_details and len(param_details.get('Parameter')) > 0 and http_auth_username is "-":
+            for param in param_details.get('Parameter'):
+                http_auth_username = param.get('Value')
+
+        param_details = client.get_parameter(Name='/' + parent_stack_name + '/http_auth_password',WithDecryption=True)
+        if 'Parameter' in param_details and len(param_details.get('Parameter')) > 0 and http_auth_password is "-":
+            for param in param_details.get('Parameter'):
+                http_auth_password = param.get('Value')
+
+        param_details = client.get_parameter(Name='/' + parent_stack_name + '/index_name',WithDecryption=True)
+        if 'Parameter' in param_details and len(param_details.get('Parameter')) > 0 and index_name is "-":
+            for param in param_details.get('Parameter'):
+                index_name = param.get('Value')
+
+    except:
+        print("Encountered an error loading credentials from SSM.")
+        traceback.print_exc()
+
+
+##################################################################################################
+    #Now put that data in ElasticCloud! 
+##################################################################################################
+    es = Elasticsearch(cloud_id=cloud_id_var, http_auth=(http_auth_username,http_auth_password))
+    es.info()
+
+    # create an index in elasticsearch, ignore status code 400 (index already exists)
+    es.indices.create(index='access-logs', ignore=400)
+    # {'acknowledged': True, 'shards_acknowledged': True, 'index': 'my-index'}
+    # datetimes will be serialized
+    # es.index(index="my-index", id=44, body={"any": "data44", "timestamp": datetime.now()})
+    
+    # Based on: 
+    # https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-aws-integrations.html#es-aws-integrations-dynamodb-es
+    print(event)
+    print("\n\n event=" + json.dumps(event))
+    event = decode_event(event)
+    print(json.dumps(event))
+
+    logEvents = event['logEvents']
+    print("Log events:\n{}".format(logEvents))
+
+    count = 0
+    errors = 0
+    print('Processing records...')
+    for record in logEvents:
+        id = record['id']
+        document = json.loads(record['message'])
+        res = es.index(index=index_name, body=document)
+        print(res)
+        print("\n\n result=" + res['result'])
+        # r = requests.put(url + id, auth=awsauth, json=document, headers=headers)
+        # if (res['status_code'] > 299):
+        if (res['result'] == 'created'):
+            count += 1
+        else:
+            print('Failed to post \ndocument:{0} \nrecord id:{1} \nresult:{2}:\n'.format(document, id, res['result']))
+            errors = 0
+    print('{} records posted to Elasticsearch in ElasticCloud.'.format(count))
+    if (errors > 0):
+        print('{} failed records not posted to Elasticsearch in ElasticCloud.'.format(count))
+
+
+    # es_body={
+    # "bucket_owner": log_entry.bucket_owner,
+    # "bucket": log_entry.bucket,
+    # "timestamp": log_entry.timestamp,
+    # "remote_ip": log_entry.remote_ip,
+    # "requester": log_entry.requester,
+    # "request_id": log_entry.request_id,
+    # "operation": log_entry.operation,
+    # "s3_key": log_entry.s3_key,
+    # "request_uri": log_entry.request_uri,
+    # "status_code": log_entry.status_code,
+    # "error_code": log_entry.error_code,
+    # "bytes_sent": log_entry.bytes_sent,
+    # "object_size": log_entry.object_size,
+    # "total_time": log_entry.total_time,
+    # "turn_around_time": log_entry.turn_around_time,
+    # "referrer": log_entry.referrer,
+    # "user_agent": log_entry.user_agent,
+    # "version_id": log_entry.version_id
+    # }
+
+    # es.index(index=index_name, body=es_body)
+
+
+
